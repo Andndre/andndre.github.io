9d086ff3da9bd97904d6d6197db8a081ecfb1f01
Andndre <agungandre687@gmail.com>
2022-09-07 20:19:53 +0800

    just redirect to https://andndre.vercel.app/

--- src/error.js
+++
@@ -1,5 +0,0 @@
-import { is404 } from './utils';
-
-if (is404()) {
-	console.log('404 page');
-}
--- src/utils.js
+++
@@ -1,9 +0,0 @@
-export function is404() {
-	const url = window.location.href;
-	console.log(url);
-	const http = new XMLHttpRequest();
-	http.open('HEAD', url, false);
-	http.send();
-	if (http.status != 404) return true;
-	else return false;
-}
